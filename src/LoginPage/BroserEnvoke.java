50d4fa3de0255f7afd6cadf6be6cc357749289ec
Akash Varma <akash.varma@cygnetinfotech.com>
2019-12-27 11:08:49 +0530

    Added test text

--- src/LoginPage/BroserEnvoke.java
+++ src/LoginPage/BroserEnvoke.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 public class BroserEnvoke {
 	static WebDriver driver;
 	public static void main(String[] args) {
-		
+		//test
 		System.setProperty("webdriver.gecko.driver", "E:/software/geckodriver-v0.16.1-win64/geckodriver.exe");
 		FirefoxProfile profile = new FirefoxProfile();
 		profile.setAcceptUntrustedCertificates(true); 
